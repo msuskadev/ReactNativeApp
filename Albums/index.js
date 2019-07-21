fe6b0b0eaed01be96c0b947e84f914a2c2b5d48c
msuskadev <msuska@gmail.com>
2019-07-22 00:57:01 +0200

    Added header and album list

--- Albums/index.js
+++ Albums/index.js
@@ -1,9 +1,13 @@
 import React from "react";
-import { AppRegistry } from "react-native";
+import { AppRegistry, View } from "react-native";
 import Header from './src/components/header';
+import AlbumList from './src/components/albumList';
 
 const AlbumsApp = () => (
-    <Header />
+    <View>
+        <Header headerText={'Albums!'} />
+        <AlbumList />
+    </View>
 );
 
 AppRegistry.registerComponent("Albums", () => AlbumsApp);
